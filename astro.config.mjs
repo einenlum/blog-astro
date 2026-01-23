c8ea8fe51c4bf8aa126bdd62ec0b7990bfddbd40
Einenlum <contact@einenlum.com>
2026-01-23 14:25:45 +0100

    update to tailwind v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 import mdx from '@astrojs/mdx';
@@ -12,12 +10,12 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
     mdx(),
     sitemap(),
   ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
   markdown: {
     shikiConfig: {
       theme: 'nord',
